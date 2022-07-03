ad58e06ae9aab6198850edfcca864a81df76d657
Oksana <nemoria@mail.ru>
2022-07-03 11:18:45 +0300

    Add code

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6
+*/
+
+Console.WriteLine("Введите число");
+string numberStr = Console.ReadLine();
+int number = int.Parse(numberStr);
+Console.WriteLine($"Третья цифра{number%10}");
+
